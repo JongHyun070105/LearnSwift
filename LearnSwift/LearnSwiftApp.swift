6048c2a6aa330b97fd7ce019660373d578d0d183
JongHyun <bobi09361@gmail.com>
2025-11-03 14:40:48 +0900

    Initial Commit

---
+++ LearnSwift/LearnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftApp.swift
+//  LearnSwift
+//
+//  Created by 종현 on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearnSwiftTests/LearnSwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  LearnSwiftTests.swift
+//  LearnSwiftTests
+//
+//  Created by 종현 on 11/3/25.
+//
+
+import Testing
+
+struct LearnSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
